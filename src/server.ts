aa0d39cc1c29ead7878867dc6342878e18a097bc
smsaifurrahman <smsaifur525@gmail.com>
2024-11-23 10:04:16 +0100

    book-shop project is initiated

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+/** @format */
+
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log('starting server');
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
